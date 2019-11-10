f197c01ff1212b1a85a92e457102861586e9bd42
JelelPro <you@example.com>
2019-11-10 20:58:59 +0100

    adding a  wrapper component

---
+++ src/Component/Wrapper/Wrapper.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt'
+import './Wrapper.css';
+
+const Wrapper = ({children, message, onCommandChange, command}) => {
+  return (
+  	console.log(children),
+    <div className='ma4 mt0'>
+      <article onClick={()=> onCommandChange(command)} className="dim br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l wide pt3 displayGrid shadow-5 center pointer">
+				{children}
+				<div>
+	        <p className='b pa1 b--black-10'>{message}</p>
+	      </div>
+	    </article>
+	  </div>
+  )
+}
+
+export default Wrapper;
