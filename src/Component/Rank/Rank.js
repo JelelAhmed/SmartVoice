59981c5d94b75e076d02518a69cc90836637271b
JelelPro <you@example.com>
2019-12-08 01:37:00 +0100

    new rank component

---
+++ src/Component/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Rank = ({name, legend}) => {
+  return (
+  	<div className='white f3'>
+	  	<div className=''>
+	  		{`${name}, The number queries you've submitted is...`}
+			</div>
+			<div className='b f2 mb2'>
+	  		{legend}
+			</div>
+			<div>
+				{"Get Smarter. Ask SmartBrain Anything!"}
+			</div>
+		</div>
+  )
+}
+
+export default Rank;
