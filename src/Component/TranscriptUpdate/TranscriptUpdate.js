97ee15a71cb262d27123b7bb763f2cee963fd7e2
jelel6 <abduljelelahmed@gmail.com>
2020-02-16 03:58:51 -0800

    deleting commented out codes

--- src/Component/TranscriptUpdate/TranscriptUpdate.js
+++ src/Component/TranscriptUpdate/TranscriptUpdate.js
@@ -8,14 +8,6 @@ const TranscriptUpdate = ({transcript}) => {
     </div>
   )
 
-
-	// return (
-	// 	<div className="mw7 content center mt4">
- //      <div className="ui black fluid card ma2 shadow-5">
- //        <div className="black f4 pa2">{transcript}</div>
- //      </div>
- //    </div>
-	// )
 }
 
 export default TranscriptUpdate;
